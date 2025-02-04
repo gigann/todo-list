717c3c0715dc861631c4a1aea8b8cc2ff7c43cf6
gigann <issacgann@gmail.com>
2025-02-04 16:53:06 +0100

    added html items

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 console.log('My code is running');
+
+var todoListItems = [];
+
+/**
+ * Adds an item to the todo list.
+ * @param {*} item
+ */
+var addItem = (item) => {
+    todoListItems.push(item);
+}
+
+var populateHTML = () => {
+
+}
